af613932d4dcd2c016a1fc3cd63af1321b791e24
Jay Mollica <jaymollica@gmail.com>
2018-02-28 20:33:09 -0800

    Update setup script to properly identify dependencies

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 echo: "info: Setting up..."
-
-if [[ ! pipenv ]]; then
+# install redis dependency
+if ! [ -x "$(command -v python3)" ]; then
+    echo "python3 required, check readme.md"
+    exit
+fi
+# install redis dependency
+if ! [ -x "$(command -v redis)" ]; then
+    echo "installing redis"
+    brew install redis
+fi
+# checking pipenv
+if ! [ -x "$(command -v pipenv)" ]; then
     echo "installing pipenv"
     sudo -H pip install --ignore-installed pipenv
 fi
@@ -12,10 +22,6 @@ virtualenv -p python3 venv
 source venv/bin/activate
 # install requirements
 pip install -r requirements.txt 
-# install redis dependency
-if [[ ! redis ]]; then
-    echo "installing redis"
-    brew install redis
-fi
+echo "updating env"
 # check for local env variables
 python -c 'import scripts.env as env; env.setup()'
