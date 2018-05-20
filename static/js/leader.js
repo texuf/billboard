4b7c38da2d5e89613c0cda3e4c0e109ee1affaa9
Austin Ellis <austinellis+github@gmail.com>
2018-05-19 18:44:49 -0700

    break out global chat from leader functionality

--- static/js/leader.js
+++ static/js/leader.js
@@ -1,34 +1,3 @@
-// Support TLS-specific URLs, when appropriate.
-if (window.location.protocol == "https:") {
-  var ws_scheme = "wss://";
-} else {
-  var ws_scheme = "ws://"
-};
-
-
-var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive");
-var outbox = new ReconnectingWebSocket(ws_scheme + location.host + "/submit");
-
-inbox.onmessage = function(message) {
-  console.log(message)
-  console.log(message.data)
-  console.log(typeof(message.data))
-  var data = JSON.parse(message.data);
-  console.log(data)
-  console.log(typeof(data))
-  $("#chat-text").append("<div ><div >" + $('<span/>').text(data.handle).html() + "</div><div >" + $('<span/>').text(data.text).html() + "</div></div>");
-  $("#chat-text").stop().animate({
-    scrollTop: $('#chat-text')[0].scrollHeight
-  }, 800);
-};
-
-inbox.onclose = function(){
-    console.log('inbox reconnecting...');
-};
-
-outbox.onclose = function(){
-    console.log('outbox reconnecting...');
-};
 
 $("#input-form-marker").on("submit", function(event) {
   event.preventDefault();
