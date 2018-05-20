b5643e5f70bba13c14de76661f0cce04030ade45
Austin Ellis <austinellis+github@gmail.com>
2018-05-19 19:45:09 -0700

    Refactor the marker code into the follower

--- static/js/follower.js
+++ static/js/follower.js
@@ -1,4 +1,37 @@
 
+//////////////////////////////////////////////////////////////////////////////
+//                build canvas 
+//////////////////////////////////////////////////////////////////////////////
+console.log('screen size', document.getElementById('qrcodeCanvasWrapper').offsetWidth, document.getElementById('qrcodeCanvasWrapper').offsetHeight)
+var qrcodeSize = Math.min(document.getElementById('qrcodeCanvasWrapper').offsetWidth, document.getElementById('qrcodeCanvasWrapper').offsetHeight) - 40
+var canvas = document.createElement('canvas');
+canvas.width  = qrcodeSize * 2;
+canvas.height = qrcodeSize * 2;
+canvas.style.width  = (qrcodeSize) + 'px';
+canvas.style.height = (qrcodeSize) + 'px';
+
+var hiroImage = new Image;
+
+hiroImage.onload = function() {
+        console.log('hiro image loaded')
+        updateARCode()
+}
+
+
+function updateARCode(){
+        document.querySelector('#arcode-container').appendChild(canvas)
+
+        var context = canvas.getContext('2d')
+        
+        context.drawImage(hiroImage, 0, 0, canvas.width, canvas.height);
+}
+
+
+function setHeroImage(url) {
+    hiroImage.src = url
+}
+
+
 
 class Follower {
 
@@ -6,8 +39,9 @@ class Follower {
         this.inboxes = []
     }
 
-    subscribe(followerID){
-        console.log("subscribeTo", followerID)
+    initialize(followerID, baseURL){
+        console.log("subscribeTo", followerID, baseURL)
+        this.baseURL = baseURL
         // Support TLS-specific URLs, when appropriate.
         if (window.location.protocol == "https:") {
           var ws_scheme = "wss://";
@@ -37,6 +71,8 @@ class Follower {
                 console.log("got message to display marker", message.marker)
                 // remove qr code from dom
                 // add marker
+                jQuery('#qrcodeCanvas').empty(); // empty removes children, remove removes the whole thing
+                setHeroImage(this.baseURL + message.marker + ".png")
                 break
             case "image":
                 console.log("got message to display image", message.image)
