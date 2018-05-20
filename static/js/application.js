04fab902284afe49672740fac2eb04249452d2ac
Austin Ellis <austinellis+github@gmail.com>
2018-05-19 18:35:55 -0700

    Wire up different message types

--- static/js/application.js
+++ static/js/application.js
@@ -35,5 +35,24 @@ $("#input-form-marker").on("submit", function(event) {
   var follower = $("#input-follower")[0].value;
   var marker   = $("#input-marker")[0].value;
   outbox.send(JSON.stringify({ channel: follower, type:"marker", marker: marker }));
-  $("#input-marker")[0].value = "";
+  // $("#input-marker")[0].value = "";
+});
+
+
+$("#input-form-image").on("submit", function(event) {
+  event.preventDefault();
+  var follower = $("#input-follower")[0].value;
+  var image   = $("#input-image")[0].value;
+  outbox.send(JSON.stringify({ channel: follower, type:"image", image: image }));
+});
+
+
+$("#input-form-position").on("submit", function(event) {
+  event.preventDefault();
+  var follower = $("#input-follower")[0].value;
+  var x   = $("#input-x")[0].value;
+  var y   = $("#input-y")[0].value;
+  var width   = $("#input-width")[0].value;
+  var height   = $("#input-height")[0].value;
+  outbox.send(JSON.stringify({ channel: follower, type:"position", x: x, y: y, width: width, height: height }));
 });
--- static/js/subscriptions.js
+++ static/js/subscriptions.js
@@ -4,10 +4,11 @@ class Subscriptions {
 
     constructor() {
         this.inboxes = []
+        this.callbacks = []
     }
 
-    add(qrCode){
-        console.log("subscribeTo", qrCode)
+    add(followerID, onMessage){
+        console.log("subscribeTo", followerID)
         // Support TLS-specific URLs, when appropriate.
         if (window.location.protocol == "https:") {
           var ws_scheme = "wss://";
@@ -15,7 +16,7 @@ class Subscriptions {
           var ws_scheme = "ws://"
         };
 
-        var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive/" + qrCode);
+        var inbox = new ReconnectingWebSocket(ws_scheme + location.host + "/receive/" + followerID);
 
         inbox.onmessage = function(message) {
           var data = JSON.parse(message.data);
@@ -24,10 +25,11 @@ class Subscriptions {
           //$("#chat-text").stop().animate({
           //  scrollTop: $('#chat-text')[0].scrollHeight
           //}, 800);
+          onMessage(data)
         };
 
         inbox.onclose = function(){
-            console.log('inbox reconnecting...', qrCode);
+            console.log('inbox reconnecting...', followerID);
         };
 
         this.inboxes.push(inbox)
